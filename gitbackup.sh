941261cdaa78552a0e5e31542e85568404be9687
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2021-09-27 11:04:55 +0100

    Voronpi autocommit from 2021-09-27 11:04:55
    Klipper on commit: c2d3a16
    
    Moonraker on commit: 81b0c0a
    
    Mainsail version: v2.0.1

---
+++ gitbackup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+export $(grep -v '^#' ~/.gh_token | xargs -0)
+
+#####################################################################
+### Please set the paths accordingly. In case you don't have all  ###
+### the listed folders, just keep that line commented out.        ###
+#####################################################################
+### Path to your config folder you want to backup
+config_folder=~/klipper_config
+
+### Path to your Klipper folder, by default that is '~/klipper'
+klipper_folder=~/klipper
+
+### Path to your Moonraker folder, by default that is '~/moonraker'
+moonraker_folder=~/moonraker
+
+### Path to your Mainsail folder, by default that is '~/mainsail'
+mainsail_folder=~/mainsail
+
+### Path to your Fluidd folder, by default that is '~/fluidd'
+#fluidd_folder=~/fluidd
+
+#####################################################################
+#####################################################################
+
+
+#####################################################################
+################ !!! DO NOT EDIT BELOW THIS LINE !!! ################
+#####################################################################
+grab_version(){
+  if [ ! -z "$klipper_folder" ]; then
+    echo "Getting klipper version"
+    cd "$klipper_folder"
+    klipper_commit=$(git rev-parse --short=7 HEAD)
+    m1="Klipper on commit: $klipper_commit"
+    cd ..
+  fi
+  if [ ! -z "$moonraker_folder" ]; then
+    echo "Getting moonraker version"
+    cd "$moonraker_folder"
+    moonraker_commit=$(git rev-parse --short=7 HEAD)
+    m2="Moonraker on commit: $moonraker_commit"
+    cd ..
+  fi
+  if [ ! -z "$mainsail_folder" ]; then
+    echo "Getting mainsail version"
+    mainsail_ver=$(head -n 1 $mainsail_folder/.version)
+    m3="Mainsail version: $mainsail_ver"
+  fi
+  if [ ! -z "$fluidd_folder" ]; then
+    echo "Getting fluidd version"
+    fluidd_ver=$(head -n 1 $fluidd_folder/.version)
+    m4="Fluidd version: $fluidd_ver"
+  fi
+}
+
+push_config(){
+  cd $config_folder
+  git pull -v
+  git add . -v
+  current_date=$(date +"%Y-%m-%d %T")
+  git commit -m "Voronpi autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+  git -c http.extraheader="AUTHORIZATION: basic $GH_TOKEN" push -v
+}
+
+grab_version
+push_config
