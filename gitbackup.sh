4e5ad7a226d3d4b744c798aed145eb9c7fea263b
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2021-09-27 11:24:55 +0100

    Voronpi autocommit from 2021-09-27 11:24:55
    Klipper on commit: c2d3a16
    
    Moonraker on commit: 81b0c0a
    
    Mainsail version: v2.0.1

--- gitbackup.sh
+++ gitbackup.sh
@@ -65,7 +65,7 @@ push_config(){
   git add . -v
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Voronpi autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git push "https://voronpi:$GH_TOKEN@github.com/richardjm/voronpi-klipper-backup.git" -v
+  git push "https://voronpi:$GH_TOKEN@github.com/richardjm/voronpi-klipper-backup.git"
 }
 
 grab_version
