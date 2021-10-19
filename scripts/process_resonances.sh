595a25dddbbd5e08f13d434bb0132604badbcd60
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2021-10-19 17:04:33 +0100

    Voronpi autocommit from 2021-10-19 17:04:33
    Klipper on commit: b806d71
    
    Moonraker on commit: c3f1b29
    
    Mainsail version: v2.0.1

--- scripts/process_resonances.sh
+++ scripts/process_resonances.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+now=$(date +"%F-%X")
 # TEST_RESONANCES AXIS=X
 # TEST_RESONANCES AXIS=Y
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /tmp/shaper_calibrate_x.png
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /tmp/shaper_calibrate_y.png
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /tmp/shaper_calibrate_x_maxsmooth02.png --max_smoothing=0.2
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /tmp/shaper_calibrate_y_maxsmooth02.png --max_smoothing=0.2
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_x-${now}.png
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_y-${now}.png
 # TEST_RESONANCES AXIS=1,1 OUTPUT=raw_data
 # TEST_RESONANCES AXIS=1,-1 OUTPUT=raw_data
-~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /tmp/resonances.png
+# ~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /tmp/resonances.png
+mv /tmp/resonances_*.csv /home/pi/klipper_config/resonances
