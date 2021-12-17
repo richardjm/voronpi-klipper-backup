888f8aa5edf71d56d8405c2c65e436a4319c7a56
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2021-12-17 08:22:41 +0000

    Backup triggered on 2021-12-17 08:22:41
    Klipper on commit: 051133f
    
    Moonraker on commit: 1ad83ce
    
    Mainsail version: v2.0.1

--- scripts/process_resonances.sh
+++ scripts/process_resonances.sh
@@ -2,8 +2,8 @@
 now=$(date +"%F-%X")
 # TEST_RESONANCES AXIS=X
 # TEST_RESONANCES AXIS=Y
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_x-${now}.png
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_y-${now}.png
+/home/pi/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_x-${now}.png
+/home/pi/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /home/pi/klipper_config/resonances/shaper_calibrate_y-${now}.png
 # TEST_RESONANCES AXIS=1,1 OUTPUT=raw_data
 # TEST_RESONANCES AXIS=1,-1 OUTPUT=raw_data
 # ~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /tmp/resonances.png
