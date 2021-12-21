8df1bbd28ab0c8646c3f8637e4de24c7cc98d828
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2021-12-21 06:53:45 +0000

    Backup triggered on 2021-12-21 06:53:45
    Klipper on commit: 051133f
    
    Moonraker on commit: 1ad83ce
    
    Mainsail version: v2.0.1

--- scripts/git_backup.sh
+++ scripts/git_backup.sh
@@ -66,7 +66,8 @@ push_config(){
   git add . -v
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Backup triggered on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git push "https://voronpi:$GH_TOKEN@github.com/richardjm/voronpi-klipper-backup.git"
+#  git push "https://voronpi:$GH_TOKEN@github.com/richardjm/voronpi-klipper-backup.git"
+  git push "git@github.com:richardjm/voronpi-klipper-backup.git"
 }
 
 grab_version
