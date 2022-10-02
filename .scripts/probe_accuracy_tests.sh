afeaa5bbd3b4da57f7ea3f3ad764287ca8a204ab
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2022-10-02 09:55:31 +0100

    Backup triggered on 2022-10-02 09:55:31
    Klipper on commit: a2482d4
    
    Moonraker on commit: 60b871a
    
    Mainsail version: v2.3.1

---
+++ .scripts/probe_accuracy_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Run all tests
+python3 /home/pi/probe_accuracy_tests/probe_accuracy_test_suite.py
+
+# Move all output for visibility via github
+mv /home/pi/probe_accuracy_tests/output/&*.png /home/pi/klipper_config/probe_accuracies
