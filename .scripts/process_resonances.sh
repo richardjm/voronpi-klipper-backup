1fe40f3a5e35afe8c738916be425ea9f36b11d62
Richard Mitchell <2081919+richardjm@users.noreply.github.com>
2022-12-31 21:56:54 +0000

    Backup triggered on 2022-12-31 21:56:54
    Klipper on commit: f57ff2c
    
    Moonraker on commit: e3bcfb5
    
    Mainsail version: v2.4.1

--- .scripts/process_resonances.sh
+++ .scripts/process_resonances.sh
@@ -12,4 +12,4 @@ echo processing y-axis
 # TEST_RESONANCES AXIS=1,1 OUTPUT=raw_data
 # TEST_RESONANCES AXIS=1,-1 OUTPUT=raw_data
 # ~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /tmp/resonances.png
-# mv /tmp/resonances_*.csv /home/pi/klipper_config/resonances
+rm /tmp/resonances_*.csv
